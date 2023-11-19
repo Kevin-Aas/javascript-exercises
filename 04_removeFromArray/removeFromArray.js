c69fc967e4ee2148ff5c718b2ecb56a009f40a05
Kevin Aslesen <kevinalexander@live.no>
2023-11-20 00:04:28 +0100

    Loop and add everything except value

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    let newArray = [];
+    for (let elmnt of array) {
+        if (elmnt != value) {
+            newArray.push(elmnt);
+        }
+    }
+    return newArray;
 };
 
 // Do not edit below this line
