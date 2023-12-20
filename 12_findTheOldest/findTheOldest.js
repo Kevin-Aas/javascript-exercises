0e58fcfd639c62f15ce301e8cf13f13e694273af
Kevin Aslesen <kevinalexander@live.no>
2023-12-20 16:28:37 +0100

    Finish

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.sort(function(a, b) {
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = new Date().getFullYear();
+        };
+        if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = new Date().getFullYear();
+        };
+        let ageA = a.yearOfDeath - a.yearOfBirth;
+        let ageB = b.yearOfDeath - b.yearOfBirth;
+        return ageA - ageB;
+    });
+    return people.at(-1);
 };
 
 // Do not edit below this line
