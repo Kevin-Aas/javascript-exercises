39af083b4e8746a08536e920bf275e48d2ca5c39
Kevin Aslesen <kevinalexander@live.no>
2023-12-20 15:36:13 +0100

    Finish exercise flawlessly

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let original = str;
+    // Format the original string
+    original = original.toLowerCase();
+    original = original.replaceAll(' ', '');
+    original = original.replaceAll('!', '');
+    original = original.replaceAll('.', '');
+    original = original.replaceAll(',', '');
+    // Reverse the formated string
+    let reversed = '';
+    for (let i=original.length-1; i>=0; i--) {
+        reversed = reversed.concat(original.charAt(i));
+    };
+    // Check if they are the same
+    if (reversed === original) {
+        return true;
+    }
+    else {
+        return false;
+    };
 };
 
 // Do not edit below this line
