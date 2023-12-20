da2e769b74c7593bc760e9a75b4340b0e4301456
Kevin Aslesen <kevinalexander@live.no>
2023-12-20 15:12:15 +0100

    Finish exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  arr.forEach(function(elmt) {
+    sum += elmt;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod = 1;
+  arr.forEach((elmt) => prod *= elmt);
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 0) {
+    return 1;
+  }
+  let res = a;
+  for (let i=a-1; i>0; i--) {
+    res *= i;
+  };
+  return res;
 };
 
 // Do not edit below this line
