6c816bb12755d50743d2d4d42005bf22860539ff
Kevin Aslesen <kevinalexander@live.no>
2023-12-20 15:52:57 +0100

    Finish

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(i) {
+    i = parseInt(i);
+    if (i < 0) {
+        return 'OOPS';
+    };
+    if (i == 1 || i == 2) {
+        return 1;
+    }
+    let fib = [1, 1];
+    for (let j=0; j<i-2; j++) {
+        fib.push(fib[j] + fib[j+1]);
+    };
+    return fib.at(-1);
 };
 
 // Do not edit below this line
